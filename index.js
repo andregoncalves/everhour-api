5a06bd9d0b5c1f098f9d584eaef7019a69cf2d60
Andre Goncalves <andre@andregoncalves.com>
2019-03-15 17:19:15 +0000

    fix: remove projects

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ module.exports = params => {
   const tasks = require("./lib/tasks")(api);
   const task = require("./lib/task")(api);
   const timer = require("./lib/timer")(api);
-  const projects = require("./lib/projects")(api);
   const reports = require("./lib/reports")(api);
 
   return { api, tasks, task, timer, projects, reports };
